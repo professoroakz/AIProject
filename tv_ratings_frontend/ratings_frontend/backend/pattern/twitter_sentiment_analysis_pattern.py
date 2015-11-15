9c865f2cb14345d442961ab072831e7382bda36b
bcooper94 <bcoopercs@live.com>
2015-11-15 14:50:44 -0800

    fixed searchShow

--- tv_ratings_frontend/ratings_frontend/backend/pattern/twitter_sentiment_analysis_pattern.py
+++ tv_ratings_frontend/ratings_frontend/backend/pattern/twitter_sentiment_analysis_pattern.py
@@ -23,32 +23,21 @@ imdb = Imdb(anonymize=True)  # to proxy requests
 # imdb = Imdb(cache=True, cache_dir='/tmp/imdbpiecache/')
 
 def getTitle(show_title):
-    title_list = imdb.search_for_title(show_title)
+    title_list = list(imdb.search_for_title(show_title))
     index = 0
     show_id = None
 
     while show_id is None:
-        if title_list[index].title == show_title:
-            show_id = title_list[index].imdb_id
+        if title_list[index][u'title'] == show_title:
+            show_id = title_list[index][u'imdb_id']
         index += 1
 
     return show_id
 
 
 def searchShow(tvshow):
-    title_list = imdb.search_for_title(tvshow)
+    title_id = getTitle(tvshow)
+    print('title: ', title_id)
+    print(imdb.get_title_reviews(title_id, max_results=15))
 
-    for show in title_list:
-        if
-    titleId = title.imdb_id
-   print(imdb.get_title_reviews(tvshow))
-
-
-
-#searchShow("The Walking Dead")
-
-# print imdb.get_title_reviews("The Walking Dead", max_results=15)
-
-print imdb.search_for_title("The Walking Dead")
-# twd_imdb_id = 'tt1520211'
-# print(imdb.get_title_reviews(twd_imdb_id, max_results=15))
+searchShow("The Walking Dead")
