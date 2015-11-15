ac69a4dc5ae3747f8004714d7ccfbe7c1c9b93bf
Oktay Gardener <oktayb@kth.se>
2015-11-15 15:30:04 -0800

    changes to nb model

--- tv_ratings_frontend/ratings_frontend/backend/pattern/twitter_sentiment_analysis_pattern.py
+++ tv_ratings_frontend/ratings_frontend/backend/pattern/twitter_sentiment_analysis_pattern.py
@@ -1,6 +1,6 @@
 from pattern.vector import Document, NB
 from pattern.db import csv
-
+import sys
 # nb = NB()
 # for review, rating in csv('reviews.csv'):
 #     v = Document(review, type=int(rating), stopwords=True)
@@ -22,6 +22,8 @@ imdb = Imdb(anonymize=True)  # to proxy requests
 # Specify optional cache directory, the default is '/tmp/imdbpiecache'
 # imdb = Imdb(cache=True, cache_dir='/tmp/imdbpiecache/')
 
+nb = NB()
+
 def getTitle(show_title):
     title_list = list(imdb.search_for_title(show_title))
     index = 0
@@ -38,32 +40,21 @@ def getTitle(show_title):
 def searchShow(tvshow):
     title_id = getTitle(tvshow)
     print('title: ', title_id)
-    reviews = imdb.get_title_reviews(title_id, max_results=100)
+    reviews = imdb.get_title_reviews(title_id, max_results=sys.maxint)
     # print(reviews[0].text)
+    print len(reviews)
     return reviews
 
 
 def naive_bayes_train(reviews):
-    nb = NB()
 
     for review in reviews:
         if review.rating is not None:
             v = Document(review.text, type=int(review.rating), stopwords=True)
             nb.train(v)
-
-            if review.rating <= 2:
-                print(nb.classify(Document('A terrible show!')))
-            elif review.rating <= 4:
-                print(nb.classify(Document('Rather disappointing.')))
-            elif review.rating <= 6:
-                print(nb.classify(Document('About average.')))
-            elif review.rating <= 8:
-                print(nb.classify(Document('A good show!')))
-            else:
-                print(nb.classify(Document('Amazing show!')))
-
-        print nb.classes
+    print nb.classes
 
 
 reviews = searchShow("The Walking Dead")
 naive_bayes_train(reviews)
+print nb.classify(Document('glenn died i fucking hate this!'))
