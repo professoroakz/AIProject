9c28ec82690267ff3dcf15115ee6065d2516caf3
bcooper94 <bcoopercs@live.com>
2015-11-16 17:20:06 -0800

    refactored to class based design

--- tv_ratings_frontend/ratings_frontend/backend/pattern/twitter_sentiment_analysis_pattern.py
+++ tv_ratings_frontend/ratings_frontend/backend/pattern/twitter_sentiment_analysis_pattern.py
@@ -4,9 +4,6 @@ from imdbpie import Imdb
 from pymongo import MongoClient
 import sys
 
-imdb = Imdb()
-imdb = Imdb(anonymize=True)  # to proxy requests
-
 
 # Creating an instance with caching enabled
 # Note that the cached responses expire every 2 hours or so.
@@ -14,85 +11,92 @@ imdb = Imdb(anonymize=True)  # to proxy requests
 # imdb = Imdb(cache=True)
 # Specify optional cache directory, the default is '/tmp/imdbpiecache'
 # imdb = Imdb(cache=True, cache_dir='/tmp/imdbpiecache/')
+class ImdbClient:
+    def __init__(self):
+        self.imdb = Imdb()
+        self.imdb = Imdb(anonymize=True)  # to proxy requests
 
-nb = NB()
+    def readFromMongo(self, show, limit):
+        # Connect to mongo
+        client = MongoClient()
 
+        # access movie stream db
+        movies = client['movieratings_stream']
 
-def getTitle(show_title):
-    title_list = list(imdb.search_for_title(show_title))
-    index = 0
-    show_id = None
+        # colletion of tweets
+        tweets = movies['tweets']
 
-    while show_id is None:
-        if title_list[index][u'title'] == show_title:
-            show_id = title_list[index][u'imdb_id']
-        index += 1
+        tweet_text = []
+        counter = 0
 
-    return show_id
+        # iterate through cursor that takes the 'limit' most recent tweets with hashtag 'show'
+        for tweet in tweets.find({'show_title': show}):  # .sort('created_at', pymongo.DESCENDING):
+            if counter < limit:
+                tweet_text.append(tweet.get("tweet_text"))
+                counter += 1
+            else:
+                break
+        return tweet_text
 
+    def getTitle(self, show_title):
+        title_list = list(self.imdb.search_for_title(show_title))
+        index = 0
+        show_id = None
 
-def searchShow(tvshow):
-    title_id = getTitle(tvshow)
-    print('title: ', title_id)
-    reviews = imdb.get_title_reviews(title_id, max_results=sys.maxint)
-    # print(reviews[0].text)
-    print len(reviews)
-    return reviews
+        while show_id is None:
+            if title_list[index][u'title'] == show_title:
+                show_id = title_list[index][u'imdb_id']
+            index += 1
 
+        return show_id
 
-def naive_bayes_train(reviews):
-    for review in reviews:
-        if review.rating is not None:
-            v = Document(review.text, type=int(review.rating), stopwords=True)
-            nb.train(v)
-    print nb.classes
-
-
-def nb_test_imdb(reviews):
-    list = []
-    for review in reviews:
-        if review.rating is not None:
-            v = Document(review.text, type=int(review.rating), stopwords=True)
-            list.append(v)
-    print nb.test(list, target=None)
-
-
-def nb_classify_tweets(tweets):
-    tweet_docs = [(nb.classify(Document(tweet)), tweet) for tweet in tweets]
-    for tweet in tweet_docs:
-        print tweet
-    # print("Doc[0] ", tweet_docs[0])
-    print("num documents: ", len(tweet_docs))
+    def searchShow(self, tvshow):
+        title_id = self.getTitle(tvshow)
+        print('title: ', title_id)
+        reviews = self.imdb.get_title_reviews(title_id, max_results=sys.maxint)
+        # print(reviews[0].text)
+        print len(reviews)
+        return reviews
 
 
-def readFromMongo(show, limit):
-    # Connect to mongo
-    client = MongoClient()
+class NBModel:
+    def __init__(self):
+        self.nb = NB()
 
-    # access movie stream db
-    movies = client['movieratings_stream']
+    def naive_bayes_train(self, reviews):
+        for review in reviews:
+            if review.rating is not None:
+                v = Document(review.text, type=int(review.rating), stopwords=True)
+                self.nb.train(v)
+        print self.nb.classes
 
-    # colletion of tweets
-    tweets = movies['tweets']
+    def nb_test_imdb(self, reviews):
+        arr = []
+        for review in reviews:
+            if review.rating is not None:
+                v = Document(review.text, type=int(review.rating), stopwords=True)
+                arr.append(v)
+        print self.nb.test(arr, target=None)
 
-    tweet_text = []
-    counter = 0
-
-    # iterate through cursor that takes the 'limit' most recent tweets with hashtag 'show'
-    for tweet in tweets.find({'show_title': show}):  # .sort('created_at', pymongo.DESCENDING):
-        if counter < limit:
-            tweet_text.append(tweet.get("tweet_text"))
-            counter += 1
-        else:
-            break
-    return tweet_text
+    def nb_classify_tweets(self, tweets):
+        tweet_docs = [(self.nb.classify(Document(tweet)), tweet) for tweet in tweets]
+        for tweet in tweet_docs:
+            print tweet
+        # print("Doc[0] ", tweet_docs[0])
+        print("num documents: ", len(tweet_docs))
 
 
-reviews = searchShow("The Walking Dead")
-naive_bayes_train(reviews)
+def main():
+    client = ImdbClient()
+    nb = NBModel()
+    reviews = client.searchShow("The Walking Dead")
+    nb.naive_bayes_train(reviews)
 
-bbtReviews = searchShow("The Big Bang Theory")
+    bbtReviews = client.searchShow("The Big Bang Theory")
 
-# nb_test_imdb(bbtReviews)
+    # nb_test_imdb(bbtReviews)
 
-nb_classify_tweets(readFromMongo("Walking Dead", 500))
+    nb.nb_classify_tweets(client.readFromMongo("Walking Dead", 500))
+
+if __name__ == "__main__":
+    main()
