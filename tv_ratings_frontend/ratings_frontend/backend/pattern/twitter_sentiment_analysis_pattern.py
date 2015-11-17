0b01a4c0001a5770030fad8170e22b0ad823a804
Oktay Gardener <oktayb@kth.se>
2015-11-16 18:39:08 -0800

    work on refactoring, statistics and hot messes

--- tv_ratings_frontend/ratings_frontend/backend/pattern/twitter_sentiment_analysis_pattern.py
+++ tv_ratings_frontend/ratings_frontend/backend/pattern/twitter_sentiment_analysis_pattern.py
@@ -54,15 +54,17 @@ class ImdbClient:
         title_id = self.getTitle(tvshow)
         print('title: ', title_id)
         reviews = self.imdb.get_title_reviews(title_id, max_results=sys.maxint)
-        # print(reviews[0].text)
         print len(reviews)
         return reviews
 
+    def getCurrentImdbRating(self, tvshow):
+        tvshowid = self.getTitle(tvshow)
+        title = self.imdb.get_title_by_id(tvshowid)
+        return float(title.rating)
 
 class NBModel:
     def __init__(self):
         self.nb = NB()
-        self.stats = Statistics()
 
     def naive_bayes_train(self, reviews):
         for review in reviews:
@@ -79,7 +81,7 @@ class NBModel:
                 arr.append(v)
         print self.nb.test(arr, target=None)
 
-    def nb_classify_tweets(self, tweets):
+    def nb_classify_tweets(self, tvshow, tweets):
         ratingSum = 0
         tweet_docs = [(self.nb.classify(Document(tweet)), tweet) for tweet in tweets]
         for tweet in tweet_docs:
@@ -87,27 +89,66 @@ class NBModel:
             print tweet
         # print("Doc[0] ", tweet_docs[0])
         print("num documents: ", len(tweet_docs))
-        self.stats.printStats(ratingSum, len(tweet_docs))
+        print("TV SHOW:::::::::::" + tvshow)
+        Statistics().printStats(tvshow, ratingSum, len(tweet_docs))
 
 class Statistics:
-    def printStats(self, sum, numItems):
+    def __init__(self):
+        self.imdb = ImdbClient()
+    def printStats(self, tvshow, sum, numItems):
         print("---------- Statistics -----------")
         print("Sum of the ratings from Twitter: ", sum)
         print("Number of classified ratings: ", numItems)
         print("Average value: ", float(sum)/numItems)
+        print("Current IMDB rating: ", self.imdb.getCurrentImdbRating(tvshow))
+
+def parse_show(show):
+    lower_show = show.lower()
+    print('Show: ', show)
+    possible_shows = ['Walking Dead', \
+            'Arrow',\
+            'Family Guy',\
+            'Big bang Theory',\
+            'South Park',\
+            'American Horror Story',\
+            'Modern Family',\
+            'Heroes Reborn']
+    if 'walking' in lower_show or 'dead' in lower_show:
+        return possible_shows[0]
+    elif lower_show == 'arrow':
+        return possible_shows[1]
+    elif lower_show == 'family guy' or 'guy' in lower_show:
+        return possible_shows[2]
+    elif 'big' in lower_show or 'bang' in lower_show or 'theory' in lower_show:
+        return possible_shows[3]
+    elif 'south' in lower_show or 'park' in lower_show:
+        return possible_shows[4]
+    elif 'american' in lower_show or 'horror' in lower_show or 'story' in lower_show:
+        return possible_shows[5]
+    elif 'modern' in lower_show:
+        return possible_shows[6]
+    elif 'heroes' in lower_show or 'reborn' in lower_show:
+        return possible_shows[7]
+
+    return 'undertermined'
 
 
-def main():
-    client = ImdbClient()
-    nb = NBModel()
-    reviews = client.searchShow("The Walking Dead")
-    nb.naive_bayes_train(reviews)
+class Classifier:
+    def __init__(self, tvshow):
+        self.tvshow = tvshow
+        self.nb = NBModel()
+        self.client = ImdbClient()
 
-    bbtReviews = client.searchShow("The Big Bang Theory")
+    def nbClassify(self):
+        reviews = self.client.searchShow(self.tvshow)
 
-    # nb_test_imdb(bbtReviews)
+        self.nb.naive_bayes_train(reviews)
 
-    nb.nb_classify_tweets(client.readFromMongo("Walking Dead", sys.maxint))
+        self.nb.nb_classify_tweets(self.tvshow, self.client.readFromMongo(parse_show(self.tvshow), 100))
+
+def main(tvshow):
+    classifier = Classifier(tvshow)
+    classifier.nbClassify()
 
 if __name__ == "__main__":
-    main()
+    main("The Walking Dead")
