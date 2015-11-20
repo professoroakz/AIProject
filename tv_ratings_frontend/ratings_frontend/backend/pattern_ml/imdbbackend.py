dedd275e9654e7b8a4f4a6f7fb5e7b76629139fe
bcooper94 <bcoopercs@live.com>
2015-11-19 22:57:47 -0800

    TVShow model up and mostly working

--- tv_ratings_frontend/ratings_frontend/backend/pattern_ml/imdbbackend.py
+++ tv_ratings_frontend/ratings_frontend/backend/pattern_ml/imdbbackend.py
@@ -2,9 +2,9 @@ from imdb import IMDb
 from imdbpie import Imdb
 from pymongo import MongoClient
 from pytvdbapi import api
-from time import sleep
-import os
+from fuzzywuzzy import fuzz
 import sys
+
 
 # get episode links with imdbPy
 # get reviews with imdbpie
@@ -30,7 +30,7 @@ class ImdbClient:
         # Connect to mongo
         client = MongoClient()
 
-        # access movie stream db
+        # access movRie stream db
         movies = client['movieratings_stream']
 
         # colletion of tweets
@@ -48,35 +48,40 @@ class ImdbClient:
                 break
         return tweet_text
 
-    def getEpisodeReviews(self, tv_episode_id):
-        pass
-        # imdb-PY
+    def getEpisodeReviews(self, episode_name):
+        return self.searchShow(episode_name)
 
-
-    def getTitle(self, show_title):
+    def get_show_id(self, show_title):
         title_list = list(self.imdb.search_for_title(show_title))
         index = 0
         show_id = None
 
-        while show_id is None:
-            result = title_list[index][u'title'].lower()
-            query = show_title.lower()
-            if result in query:
-                print title_list
-                show_id = title_list[index][u'imdb_id']
+        while index < len(title_list) and show_id is None:
+            if title_list[index] is not None:
+                result = title_list[index][u'title'].lower()
+                query = show_title.lower()
+                # if result in query:
+                if fuzz.ratio(result, query) >= 90:
+                    # print title_list
+                    show_id = title_list[index][u'imdb_id']
             index += 1
         return show_id
 
     def searchShow(self, tvshow):
-        print tvshow
-        title_id = self.getTitle(tvshow)
-        if tvshow is not self.tvshow:
+        title_id = self.get_show_id(tvshow)
+        reviews = []
+        print(tvshow)
+
+        if title_id is not None and title_id != '':
             reviews = self.imdb.get_title_reviews(title_id, max_results=sys.maxint)
             title = self.imdb.get_title_by_id(title_id)
+        else:
+            print("Invalid show id")
+
         return reviews
 
     def getCurrentImdbRating(self, tvshow):
-        tvshowid = self.getTitle(tvshow)
+        tvshowid = self.get_show_id(tvshow)
         title = self.imdb.get_title_by_id(tvshowid)
         return float(title.rating)
 
@@ -87,10 +92,11 @@ class ImdbClient:
         for x in range(1, len(show)):
             season = show[x]
             for y in range(1, len(season) + 1):
-                if season[y].EpisodeName is not None:
+                if season[y].EpisodeName is not None and season[y].EpisodeName != '':
                     res.append(season[y].EpisodeName)
         return res
 
+    # episode names for a specific season of tvshow
     def get_specific_episode_names(self, tvshow, season):
         result = self.db.search(tvshow, 'en')
         show = result[0]
@@ -102,13 +108,45 @@ class ImdbClient:
                 res.append(season[x].EpisodeName)
         return res
 
-    def get_all_episodes(self, episodelist, tvshow):
+    def get_all_episode_reviews(self, episodelist, tvshow):
+        reviews = []
         for episode in episodelist:
-            currEpisode = episode + " " + tvshow
-            reviews = []
-            reviews.append(searchshow(currEpisode))
-        #call searchshow for each
+            curEpisode = episode + " " + tvshow
+            reviews.append(self.searchShow(curEpisode))
+            # call searchshow for each
+
+        print("Episodes:\n" + str(reviews))
+        return reviews
+
 
 # get list of all episode names given a tv show
 # create review list, for each episode name, call searchshow append
 # call method that trains
+
+class TVShow:
+    def __init__(self, show_title):
+        self.client = ImdbClient()
+        self.title = show_title
+        self.show_id = None
+        self.episodes = None
+
+    def _get_episodes(self):
+        if self.episodes is None:
+            self.episodes = self.client.get_all_episode_names(self.title)
+        return self.episodes
+
+    def get_show_reviews(self):
+        return self.client.searchShow(self.title)
+
+    def get_all_episode_reviews(self):
+        reviews = {}
+        for episode in self._get_episodes():
+            reviews[episode] = self.get_episode_reviews(episode)
+        return reviews
+
+    def get_episode_reviews(self, episode_name):
+        return self.client.searchShow(episode_name)
+
+if __name__ == "__main__":
+    tv = TVShow("The Walking Dead")
+    tv.get_all_episode_reviews()
